08f28dc686f84600d11b0a7552575c714deb4e55
Royce Lee <royceleeks@gmail.com>
2024-06-30 04:09:07 +0800

    Day 4 - iterating on day 3
    Nested if statement
    clean up code to make it shorter

---
+++ day-3-and-4-cinema-cashier.py
@@ -0,0 +1,37 @@
+# # < 13 come back with your parents
+# # 13 - 21 student, pay student price
+# # 21 - 65  adult, pay adult price
+# # 65+  senior, free
+
+# print("Welcome to the cinema!")
+# print("What is your age?")
+# age = input()
+# age = int(age)
+
+# if age < 13:
+#     print("You're too young! Come back with your parents.")
+# elif age >= 13 and age < 21:
+#     print("You're a student, please pay student price: $6.")
+# elif age >= 21 and age < 65:
+#     print("You're an adult, please pay adult price: $12.")
+# else:
+#     print("You're a senior, it's free for you.")
+
+
+# version 2
+print("Welcome to the cinema!")
+print("What is your age?")
+age = input()
+
+if age:
+    age = int(age)
+    if age >= 65:
+        print("You're a senior, it's free for you.")
+    elif age >= 21:
+        print("You're an adult, please pay adult price: $12.")
+    elif age >=13:
+         print("You're a student, please pay student price: $6.")
+    else:
+        print("You're too young! Come back with your parents.")
+else:
+    print("You did not tell me your age!!")
