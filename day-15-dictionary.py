d2a0504bc9c0890cc87170067b80615b3d686e3c
Royce Lee <royceleeks@gmail.com>
2024-07-14 13:59:08 +0800

    day-15-dictionary

---
+++ day-15-dictionary.py
@@ -0,0 +1,37 @@
+# dictionary + access data
+person = {"name": "royce", "location": "earth", "race": "human"}
+print(person["name"])
+print(person["location"])
+
+#iterating over dictionary. key value pair
+#key option 1:
+for k in person:
+    print(k)
+# key option 2:
+for k in person.keys():
+    print(k)
+#value
+for v in person.values():
+    print(v)
+#items
+for k,v in person.items():
+    print(f"key is {k},value is {v}")
+
+
+#shopping list
+from random import choice
+
+item = choice(["fish", "chicken", "milk", "egg", "plants", "watermelon", "apple", "pear", "muffins", "rice"])
+
+shopping_list = {
+    "fish": 3,
+    "chicken": 4,
+    "milk": 2,
+    "egg": 12
+}
+
+print(f"{item} is here!")
+if item in shopping_list:
+    print(f"I need {shopping_list[item]} {item}")
+else:
+    print("I do not need this")
